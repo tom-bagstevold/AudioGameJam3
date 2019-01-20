01876ca12ee7993011df7ec9e808143f4a05e137
tom-bagstevold <tbagstevold@gmail.com>
2019-01-20 21:35:15 +0100

    More Audio Browsing

--- BlindNinja/Blind Ninja/Assets/Scripts/AudioBrowsingManager.cs
+++ BlindNinja/Blind Ninja/Assets/Scripts/AudioBrowsingManager.cs
@@ -9,6 +9,7 @@ public class AudioBrowsingManager : MonoBehaviour
     public AudioMixer masterMixer;
 
     [Header("Audio Mixer Group")]
+    public AudioMixerGroup selectedGroup;
     public AudioMixerGroup[] audioGroups;
     public float[] WetValues;
 
@@ -30,19 +31,24 @@ public class AudioBrowsingManager : MonoBehaviour
     [Header("Current Selection")]
     public int currentMixerGroup;
     public int currentAudioSource;
+    int previousAudioSource;
 
     bool activated;
+    bool listeningSingleSources;
+    int currentMixerGroupLength;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentMixerGroupLength = roomToneSources.Length - 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //Activation
         if(Input.GetKeyDown(KeyCode.LeftShift) && !activated)
         {
             InitiateBrowsing(true);
@@ -53,7 +59,16 @@ public class AudioBrowsingManager : MonoBehaviour
             InitiateBrowsing(false);
             activated = false;
             currentMixerGroup = 0;
+
+            //roomToneSources[previousAudioSource].outputAudioMixerGroup = audioGroups[0];
+            //ambienceToneSources[previousAudioSource].outputAudioMixerGroup = audioGroups[1];
+            specificFlavorSources[previousAudioSource].outputAudioMixerGroup = audioGroups[2];
+            //propsSources[previousAudioSource].outputAudioMixerGroup = audioGroups[3];
+            currentAudioSource = 0;
+            previousAudioSource = 0;
         }
+
+        //Browsing through layers
 
         if(Input.GetKeyDown(KeyCode.UpArrow) && activated)
         {
@@ -83,7 +98,27 @@ public class AudioBrowsingManager : MonoBehaviour
             }
         }
 
+        //Browsing through sounds on layer
 
+        if(Input.GetKeyDown(KeyCode.RightArrow) && activated)
+        {
+            
+
+            if(currentAudioSource <= currentMixerGroupLength)
+            {
+                SelectClip(currentAudioSource);
+                previousAudioSource = currentAudioSource;
+                currentAudioSource += 1;
+            }
+            else if(currentAudioSource > currentMixerGroupLength)
+            {
+                currentAudioSource = 0;
+                previousAudioSource = 0;
+                SelectClip(currentAudioSource);
+            }
+            
+
+        }
 
     }
 
@@ -118,6 +153,7 @@ public class AudioBrowsingManager : MonoBehaviour
             masterMixer.SetFloat("AmbienceWet", 0f);
             masterMixer.SetFloat("SpecificFlavorsWet", 0f);
             masterMixer.SetFloat("PropsWet", 0f);
+            currentMixerGroupLength = roomToneSources.Length - 1;
         }
 
         if (groupSelected == 2)
@@ -126,6 +162,7 @@ public class AudioBrowsingManager : MonoBehaviour
             masterMixer.SetFloat("RoomToneWet", 0f);
             masterMixer.SetFloat("SpecificFlavorsWet", 0f);
             masterMixer.SetFloat("PropsWet", 0f);
+            currentMixerGroupLength = ambienceToneSources.Length - 1;
         }
 
         if (groupSelected == 3)
@@ -134,6 +171,7 @@ public class AudioBrowsingManager : MonoBehaviour
             masterMixer.SetFloat("RoomToneWet", 0f);
             masterMixer.SetFloat("AmbienceWet", 0f);
             masterMixer.SetFloat("PropsWet", 0f);
+            currentMixerGroupLength = specificFlavorSources.Length - 1;
         }
 
         if (groupSelected == 4)
@@ -142,12 +180,44 @@ public class AudioBrowsingManager : MonoBehaviour
             masterMixer.SetFloat("RoomToneWet", 0f);
             masterMixer.SetFloat("AmbienceWet", 0f);
             masterMixer.SetFloat("SpecificFlavorsWet", 0f);
+            currentMixerGroupLength = propsSources.Length - 1;
         }
 
     }
 
-    void SelectClip()
+    void SelectClip(int sourceNum)
     {
-        
+        if(currentMixerGroup == 1)
+        {
+            masterMixer.SetFloat("RoomToneWet", 0f);
+            roomToneSources[previousAudioSource].outputAudioMixerGroup = audioGroups[0];
+            roomToneSources[currentAudioSource].outputAudioMixerGroup = selectedGroup;
+            
+        }
+
+        else if (currentMixerGroup == 2)
+        {
+            masterMixer.SetFloat("AmbienceWet", 0f);
+            ambienceToneSources[previousAudioSource].outputAudioMixerGroup = audioGroups[1];
+            ambienceToneSources[currentAudioSource].outputAudioMixerGroup = selectedGroup;
+            
+        }
+
+        else if (currentMixerGroup == 3)
+        {
+            masterMixer.SetFloat("SpecificFlavorsWet", 0f);
+            specificFlavorSources[previousAudioSource].outputAudioMixerGroup = audioGroups[2];
+            specificFlavorSources[currentAudioSource].outputAudioMixerGroup = selectedGroup;
+            
+        }
+
+        else if (currentMixerGroup == 4)
+        {
+            masterMixer.SetFloat("PropsWet", 0f);
+            propsSources[previousAudioSource].outputAudioMixerGroup = audioGroups[3];
+            propsSources[currentAudioSource].outputAudioMixerGroup = selectedGroup;
+            
+        }
+
     }
 }
