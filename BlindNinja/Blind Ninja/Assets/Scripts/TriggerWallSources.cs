f9683be69ce98fc0f8043c06512d4a70c7cd89d2
tom-bagstevold <tbagstevold@gmail.com>
2019-02-27 14:59:00 +0100

    Parkour Pt 1 complete

--- BlindNinja/Blind Ninja/Assets/Scripts/TriggerWallSources.cs
+++ BlindNinja/Blind Ninja/Assets/Scripts/TriggerWallSources.cs
@@ -19,7 +19,10 @@ public class TriggerWallSources : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            TriggerClips();
+        }
     }
 
     public void TriggerClips2()
